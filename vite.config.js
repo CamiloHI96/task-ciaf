8fb88aff114acc252b96452c19c3e5cf0c47a78a
Camilo Hernandez <camilohi.1196@gmail.com>
2025-11-05 15:49:12 -0500

    Revert "Update vite.config.js"
    This reverts commit a48286dcd8f12ab03c693fd15fdcd56044c4ed0b.

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración universal para Android + Pages
 export default defineConfig(({ mode }) => {
+
   const env = loadEnv(mode, process.cwd())
 
   return {
     plugins: [react()],
     base: env.VITE_BASE_PATH || './',
     build: {
-      outDir: 'dist',
-    },
+      outDir: 'dist'
+    }
   }
 })
