fde87419bcf8429045e9f18f4f2d77c7be30a16f
Camilo Hernandez <camilohi.1196@gmail.com>
2025-11-05 00:16:10 -0500

    Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', // ← ¡MUY IMPORTANTE para que funcione en Android!
-  build: {
-    outDir: 'dist'
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || './',
+    build: {
+      outDir: 'dist'
+    }
   }
-});
+})
